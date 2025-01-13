ed07b27d3154b05c903cc60acdc707673b6e5a6f
ChetanSrikant <chetansmandiga@gmail.com>
2025-01-13 17:48:40 +0530

    validation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/user', userRouter);
 
 app.listen(3000, () => {
