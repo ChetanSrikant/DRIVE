9528173a4775c494c601ee4a43ac2e374b775574
ChetanSrikant <chetansmandiga@gmail.com>
2025-01-14 01:31:33 +0530

    home-page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter = require('./routes/user.routes');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index.routes');
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/user', userRouter);
+app.use('/', indexRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
